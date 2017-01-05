109767c1a323bc8ff86ff4219200f84952d217dc
Zaid <zaliaslab@gmail.com>
2017-01-05 23:01:43 +0000

    Working on c9.io

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var useragent = require('express-useragent');
+
+app.use(useragent.express());
+app.get('/', function(req, res){
+  var result = {
+    "ipaddress" : req.headers['x-forwarded-for'], //req.connection.remoteAddress,
+    "language" : req.acceptsLanguages()[0],
+    "os" : req.useragent.os
+  }
+  res.send(result);
+});
+app.listen(8080);
